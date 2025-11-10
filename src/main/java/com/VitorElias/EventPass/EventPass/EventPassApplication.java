c58b63cb7b50f2804a377259f52fe8e25a57c5c0
VitorElias <vitorepbf2004@gmail.com>
2025-11-10 15:44:51 -0300

    Primeiro commit do projeto EventPass

---
+++ src/main/java/com/VitorElias/EventPass/EventPass/EventPassApplication.java
@@ -0,0 +1,13 @@
+package com.VitorElias.EventPass.EventPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VitorElias/EventPass/EventPass/EventPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VitorElias.EventPass.EventPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
